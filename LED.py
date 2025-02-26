7a14da7ffb9a80cc844e105f5a887f1280e7bd3b
Hou Xinxuan <houxinxuan2004@gmail.com>
2025-02-26 09:56:40 +0000

    changes

--- LED.py
+++ LED.py
@@ -2,12 +2,17 @@ from machine import Pin
 import time
 LED_PIN = 25  # according to documentation, not sure
 led = Pin(LED_PIN, Pin.OUT)
-def turn_led_on():
+def turn_led_on(on=True):
     delay_time = 5
     print(f"Waiting for {delay_time} seconds before turning on the LED...")
     time.sleep(delay_time)
-    led.value(1) 
-    print("LED is ON now!")
+    while on:
+        
+        led.value(1)
+        time.sleep(1)
+        led.value(0)
+        time.sleep(1)
+        
 
 def turn_led_off():
     # time_start=time.time()
@@ -17,5 +22,6 @@ def turn_led_off():
     #         print("LED is OFF now!")
     #         break
     time.sleep(8)
+    turn_led_on(False)
     led.value(0) 
     print("LED is OFF now!")
--- colour_detector.py
+++ colour_detector.py
@@ -7,9 +7,9 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tcs34725.TCS34725(i2c)
 
 while True:
-    # temp = sensor.color_temperature
-    # lux = sensor.lux
     print('Color: ({0}, {1}, {2})'.format(*sensor.color_rgb_bytes))
-    # print('Temperature: {0}K'.format(sensor.color_temperature))
-    # print('Lux: {0}'.format(sensor.lux))
-    time.sleep(1)
+    
+# red
+# green
+# blue
+# yellow
