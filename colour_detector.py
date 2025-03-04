7e974a8f87b9228290bc5a2e381b1c760a2b90e5
Bzd <stl46@cam.ac.uk>
2025-03-04 09:15:31 +0000

    Color detector

--- colour_detector.py
+++ colour_detector.py
@@ -2,9 +2,32 @@ import time
 import board
 import busio
 import adafruit_tcs34725
+from machine import Pin, PWM, I2C, ADC
+from vl53l0x import VL53L0X
+from tcs34725 import TCS34725
 
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_tcs34725.TCS34725(i2c)
+# colour sensor
+i2c_bus = I2C(0, sda=Pin(8), scl=Pin(9),freq=50000)
+print("I2C scan:", [hex(addr) for addr in i2c_bus.scan()])
+tcs = TCS34725(i2c_bus)
+
+# TOF sensor
+sda = Pin(8)
+scl = Pin(9)
+id = 0
+i2c = I2C(id=id, sda=sda, scl=scl)
+# Create a VL53L0X object
+tof = VL53L0X(i2c)
+budget = tof.measurement_timing_budget_us
+print("Budget was:", budget)
+tof.set_measurement_timing_budget(40000)
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 12)
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 8)
+
+# i2c = busio.I2C(board.SCL, board.SDA)
+# sensor = adafruit_tcs34725.TCS34725(i2c)
 def detect_colour():
     color = sensor.color_rgb_bytes
     red=color[0]
@@ -22,3 +45,4 @@ def detect_colour():
         return "invalid colour"
         
     # print('Color: ({0}, {1}, {2})'.format(*sensor.color_rgb_bytes))
+
