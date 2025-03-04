7302426bed9cf9f71f19e07d62533367035c3b29
Bzd <stl46@cam.ac.uk>
2025-03-04 18:04:22 +0000

    Import changes

--- graph.py
+++ graph.py
@@ -1,12 +1,12 @@
-from collections import deque, defaultdict
+import utime
 from machine import Pin
-from motor import Motor
+from MOTOR import Motor
 from main import location
 
-line_left = Pin(21, Pin.IN)
-line_right = Pin(20, Pin.IN)
-junction_left = Pin(19, Pin.IN)
-junction_right = Pin(18, Pin.IN)
+line_left = Pin(13, Pin.IN)
+line_right = Pin(11, Pin.IN)
+junction_left = Pin(12, Pin.IN)
+junction_right = Pin(10, Pin.IN)
 
 motor=Motor()
 
@@ -36,30 +36,35 @@ routes = {
 
 # Build graph
 def build_graph():
-    graph = defaultdict(list)
+    graph = {}
     for line in routes.values():
         for i in range(len(line) - 1):
             node1, node2 = line[i], line[i + 1]
+            if node1 not in graph:
+                graph[node1] = []
+            if node2 not in graph:
+                graph[node2] = []
             graph[node1].append(node2)
             graph[node2].append(node1)
     return graph
 
 # Breath first search to find the shortest path
 def bfs_path(graph, start, end):
-
     # Queue stores (current_node, path)
-    queue = deque([(start, [start])])  
+    queue = [(start, [start])]  
     # Set of visited nodes
     visited = set()
 
     while queue:
-        current, path = queue.popleft()
+        # Pop the first element from the queue 
+        current, path = queue.pop(0)
         if current == end:
             return path  # Return the path if the end node is reached
         if current not in visited:
             visited.add(current)
             for neighbor in graph[current]:
                 if neighbor not in visited:
+                    # Append the neighbor and the updated path to the queue
                     queue.append((neighbor, path + [neighbor]))
     return None  # If no path is found
 
@@ -71,6 +76,7 @@ def shortest_route(A, B):
     return bfs_path(graph, A, B)
 
 def line_tracking():
+    global last_junction_time
     while True:
         if line_left.value() == 1:
             motor.forward(0)
@@ -80,11 +86,14 @@ def line_tracking():
             motor.forward()
         
         if junction_left.value() == 1 or junction_right.value() == 1:
+#             current_time = utime.ticks_ms()  
+#             if utime.ticks_diff(current_time, last_junction_time) >= 200:
+#                 last_junction_time = current_time
             return
 
     
 def follow_path(path):
-    # obtains unit vector from current location to next
+    # Helper function to get unit direction from p1 to p2
     def get_direction(p1, p2):
         dx = p2[0] - p1[0]
         dy = p2[1] - p1[1]
@@ -97,12 +106,28 @@ def follow_path(path):
     direction_order = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     direction_map = {d: idx for idx, d in enumerate(direction_order)}
     
-    # moves to path[1]
+    # Move to path[1] initially
     prev_dir = get_direction(path[0], path[1])
     line_tracking()
+    global location
+    location = path[1] 
 
-    # orientate to next point, then moves forward
-    for i in range(1, len(path) - 1):
+    # Iterate over all points except the first one
+    for i in range(1, len(path)):
+        if i == len(path) - 1:
+            # Last node: turn but do not move
+            current_dir = get_direction(path[i-1], path[i])
+            
+            prev_idx = direction_map[prev_dir]
+            current_idx = direction_map[current_dir]
+            diff = (current_idx - prev_idx) % 4
+            
+            if diff == 1:
+                motor.right()  # Turn right
+            elif diff == 3:
+                motor.left()   # Turn left
+            return 
+
         current_dir = get_direction(path[i], path[i+1])
         
         prev_idx = direction_map[prev_dir]
@@ -110,18 +135,19 @@ def follow_path(path):
         diff = (current_idx - prev_idx) % 4
         
         if diff == 1:
-            motor.right() #right
+            motor.right()  # Turn right
         elif diff == 3:
-            motor.left() #left
+            motor.left()   # Turn left
         
         line_tracking()
-
+        location = path[i+1]
         prev_dir = current_dir
+        return
 
 # Moves from current location to input location, picks up box and pivots 180 degrees
-def collect(num):
+def collect(location, num):
     path = shortest_route(location, collections_points[num])
-
+    
     follow_path(path)
         
     # Picks up block
@@ -142,9 +168,9 @@ def deposit(color):
     follow_path(path)
 
     # Drop off block
-
+    
     # turn 180 degrees
     motor.back()
 
 # print("Shortest path from", node_D2, "to", node_D, ":", shortest_route(node_D2, node_D))
-line_tracking()
+# line_tracking()
