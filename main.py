62f92f0c428755a71fa762e61b2de8cd2d9a922b
Bzd <stl46@cam.ac.uk>
2025-03-05 09:35:18 +0000

    main

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import time
-import threading
+import utime
 from machine import Pin
 from LED import turn_led_off, turn_led_on
 from colour_detector import detect_colour
+from graph import collect, deposit
 
-start = time.time()
+tasks = [0,1,2,3]
+
+start = utime.time()
 location = (0,0)
 turn_led_on()
+
+def main():
+    while utime.time()-start<270:
+        for i in tasks:
+            end = utime.time()
+            if end-start>270:
+                break
+            collect(i)
+            color = detect_colour()
+            deposit(color)
+            
+    turn_led_off()
 
 button=Pin(12,Pin.IN)
 while True:
     if button.value()==1:
-        #main()
+        main()
         
 
-tasks = [0,1,2,3]
-
-blink_thread = threading.Thread(target=blink_led, daemon=True)
-blink_thread.start()
-
-while time.time()-start<270:
-    for i in tasks:
-        end = time.time()
-        if end-start>270:
-            break
-        collect(i)
-        color = detect_colour()
-        deposit(color)
-        
-turn_led_off()
