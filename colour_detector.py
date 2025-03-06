bb04ad9af235cee31cc11faba54ad7461af14d54
Hou Xinxuan <houxinxuan2004@gmail.com>
2025-03-06 09:52:07 +0000

    colour

--- colour_detector.py
+++ colour_detector.py
@@ -3,6 +3,7 @@ from MOTOR import Motor
 from machine import Pin, PWM, I2C, ADC
 from vl53l0x import VL53L0X
 from tcs34725 import TCS34725, html_rgb
+from time import sleep_ms
 
 # colour sensor
 i2c_bus = I2C(1, sda=Pin(18), scl=Pin(19))
@@ -10,47 +11,77 @@ TCS34725_ADDRESS = 0x29
 print("I2C scan:", [hex(addr) for addr in i2c_bus.scan()])
 tcs = TCS34725(i2c_bus)
 motor = Motor()
-
-# colour sensor
-i2c_bus = I2C(0, sda=Pin(8), scl=Pin(9),freq=50000)
-print("I2C scan:", [hex(addr) for addr in i2c_bus.scan()])
-tcs = TCS34725(i2c_bus)
+def scan_sensor(i2c_bus):
+    devices = i2c_bus.scan()
+    if TCS34725_ADDRESS in devices:
+        print("TCS34725 sensor detected at address 0x{:02X}".format(TCS34725_ADDRESS))
+        return True
+    else:
+        print("TCS34725 sensor not detected.")
+        return False
 
 # TOF sensor
-sda = Pin(18)
-scl = Pin(19)
-id = 1
-i2c = I2C(id=id, sda=sda, scl=scl)
+#sda = Pin(18)
+#scl = Pin(19)
+#id = 1
+#i2c = I2C(id=id, sda=sda, scl=scl)
 # Create a VL53L0X object
-tof = VL53L0X(i2c)
-budget = tof.measurement_timing_budget_us
-print("Budget was:", budget)
-tof.set_measurement_timing_budget(40000)
-tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
-tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 12)
-tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
-tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 8)
+#tof = VL53L0X(i2c)
+#budget = tof.measurement_timing_budget_us
+# print("Budget was:", budget)
+# tof.set_measurement_timing_budget(40000)
+# tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
+# tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 12)
+# tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
+# tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 8)
 
+        
+
+# Function to scan I2C devices
+def scan_sensor(i2c_bus):
+    devices = i2c_bus.scan()
+    if TCS34725_ADDRESS in devices:
+        print("TCS34725 sensor detected at address 0x{:02X}".format(TCS34725_ADDRESS))
+        return True
+    else:
+        print("TCS34725 sensor not detected.")
+        return False
+
+# Function to test the sensor and get readings
+# def test_sensor():
+#     if scan_sensor(i2c_bus):
+
+#         tcs = TCS34725(i2c_bus)
+#         print('Raw data: {}'.format(tcs.read('raw')))
+#         print('RGB data: {}'.format(tcs.read('rgb')))
+#         print(tcs.read(raw=False))
+#     else:
+#         print("Unable to initialize sensor.")
+# while True:
+#     print(tcs.read('raw'))
+#     sleep_ms(500)
+#     
 def detect_colour():
     color = html_rgb(tcs.read('raw'))
     print(color)
     red=color[0]
     green=color[1]
     blue=color[2]
-    if red > 128 and green > 128:
+    if red > 10 and green > 20 and blue<10:
+#         print('yellow')
         return "yellow"
-    elif red > 128:
+    elif red > 20:
+#         print('red')
         return "red"
-    elif blue > 128:
+    elif blue > 20:
+#         print('blue')
         return "blue"
-    elif green > 128:
+    elif green > 10 and blue>10:
+#         print('green')
         return "green"
     else:
-        return "invalid colour"
-        
-print(detect_colour())
-
-    
+#         print('invalid')
+        return "invalid colour"    
 
 def pickup():
     while (tof.ping()-36) > 10:
